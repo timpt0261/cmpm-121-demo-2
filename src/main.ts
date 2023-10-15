10581022a75dbd377c5871440c98a6591d13eef4
timpt0261 <rchavez0215@gmail.com>
2023-10-14 23:10:22 -0700

    Completed Step 1

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game";
+const gameName = "SketchPad";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.style.background = "lightgrey";
+canvas.style.boxShadow = "black 1rem 1rem 10px";
+canvas.style.borderRadius = "45px";
+canvas.style.border = "3px solid black";
+app.append(canvas);
