228179bebf801770274a6570773689e2aa494483
timpt0261 <rchavez0215@gmail.com>
2023-10-15 18:36:00 -0700

    Completed Step 2

--- src/main.ts
+++ src/main.ts
@@ -18,3 +18,42 @@ canvas.style.boxShadow = "black 1rem 1rem 10px";
 canvas.style.borderRadius = "45px";
 canvas.style.border = "3px solid black";
 app.append(canvas);
+
+const ctx = canvas.getContext("2d");
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    ctx?.beginPath();
+    ctx?.moveTo(cursor.x, cursor.y);
+    ctx?.lineTo(e.offsetX, e.offsetY);
+    ctx?.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+app.append(clearButton);
+
+clearButton.addEventListener("click", clear);
+
+const origin = { x: 0, y: 0 };
+
+function clear() {
+  if (canvas && ctx) {
+    ctx.clearRect(origin.x, origin.y, canvas.width, canvas.height);
+  }
+}
